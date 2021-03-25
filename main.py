9a98d1fd25a7ba3089e364ec6a6ab283e818565c
Naveed Chowdhury <naveedc6@gmail.com>
2021-03-25 19:32:45 -0400

    updates

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import http.client
 import api_interaction as API
 import time
 
+
+
+
 conn = http.client.HTTPSConnection("www.notexponential.com")
 payload = ''
 headers = {
@@ -12,19 +15,9 @@ headers = {
   'userId': '1071'
 }
 gameId = "2127"
-#get_moves(conn, payload, headers, gameId, "30")
+teanId = '1260'
 
 
-
-API.get_moves(conn, payload, headers, gameId,'1')
-lastMove = ['6','6']
-
-moves = API.get_moves(conn, payload, headers, gameId,'1')
-moves=json.loads(moves)
-x = moves['moves'][0]['moveX']
-y = moves['moves'][0]['moveY']
-symbol = moves['moves'][0]['symbol']
-timeId = '1260'
 
 while True :
     #getting moves'
@@ -39,7 +32,7 @@ while True :
     else:
         move = 'X'
     boardString =  API.get_board_string(conn, payload, headers, gameId)
-    
+
     # boardString =  json.loads(boardString)
     # boardString = boardString["output"]
     # board = boardStringTo2DArray(boardString)
@@ -47,4 +40,4 @@ while True :
     #     print(row)
     # move = getNextBestMove(board, move)
     # move = str(move[0]) + ',' + str(move[1])
-    API.make_move(conn, payload, headers,timeId, move, gameId)
+    API.make_move(conn, payload, headers,teanId, move, gameId)
